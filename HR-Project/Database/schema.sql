a1c33830c0195c90ea31a856f983750aa1340cf0
Yabaiii1107 <jenrick1107hablanavarro@gmail.com>
2026-06-05 02:16:12 +0800

    Update schema.sql

--- HR-Project/Database/schema.sql
+++ HR-Project/Database/schema.sql
@@ -1,13 +1,17 @@
-CREATE DATABASE hr_db;
-
 USE hr_db;
 
 CREATE TABLE applicants (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(100),
-    last_name VARCHAR(100),
+
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
     middle_name VARCHAR(100),
-    email VARCHAR(100),
-    contact VARCHAR(20),
-    password VARCHAR(255)
+    birth_date DATE,
+   
+    email VARCHAR(100) NOT NULL UNIQUE,
+    contact VARCHAR(20) NOT NULL UNIQUE,
+
+    password VARCHAR(255) NOT NULL,
+
+    registration_date DATETIME DEFAULT CURRENT_TIMESTAMP
 );
