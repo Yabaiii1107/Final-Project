804e1e741cf281190e4e023b9f84beec51ed1230
Yabaiii1107 <jenrick1107hablanavarro@gmail.com>
2026-06-11 03:31:07 +0800

    Enhance HRDashboard with new features and connection string
    Added connection string and additional button functionalities for applicants, logout, and job vacancies management.

--- HR-Project/HR-System/HRDashboard.cs
+++ HR-Project/HR-System/HRDashboard.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +12,12 @@ namespace HR_Project.HR_System
 {
     public partial class HRDashboard : Form
     {
+
         public string UserRole { get; set; }
 
         public string UserName { get; set; }
+
+        string connectionString = "server=127.0.0.1;port=3306;uid=root;pwd=031107Navarro;database=hr_db;";
 
         public HRDashboard()
         {
@@ -23,7 +26,41 @@ namespace HR_Project.HR_System
 
         private void btnApplicants_Click(object sender, EventArgs e)
         {
+            HRApplicants applicants = new HRApplicants();
+            applicants.FormClosed += (s, args) => this.Show();
+            this.Hide();
+            applicants.Show();
+        }
 
+        private void btnProfilePageClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnMyDocumentsLogout_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+                "Are you sure you want to logout?",
+                "Logout",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                Login login = new Login();
+
+                login.Show();
+
+                this.Hide();
+            }
+        }
+
+        private void btnJobVacanciesManagement_Click(object sender, EventArgs e)
+        {
+            JobVacancyManagement jobForm = new JobVacancyManagement();
+            jobForm.FormClosed += (s, args) => this.Show();
+            this.Hide();
+            jobForm.Show();
         }
     }
 }
